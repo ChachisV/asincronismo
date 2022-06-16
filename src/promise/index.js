323f7becea4f82c219da68b2a7ec2483b11d25cf
Jaime Andres Vanegas <javanmot@gmail.com>
2022-06-15 22:22:23 -0500

    entiendo un poco mas de las promesas en la clase de promesas

---
+++ src/promise/index.js
@@ -0,0 +1,38 @@
+const somethingWillHappen = () => {
+    return new Promise((resolve, reject) => {
+        if(true){
+            resolve("hey!");
+        }else {
+            reject("woops!");
+        }
+    });
+};
+
+somethingWillHappen()
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
+const somethingWillHappen2 = () =>{
+    return new Promise((resolve, reject) => {
+        if(true) {
+            setTimeout(() => {
+                resolve("true");
+            }, 2000)
+        } else{
+            const error = new Error("whoops");
+            reject(error);
+        }
+    })
+}
+
+somethingWillHappen2()
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
+
+Promise.all([somethingWillHappen(), somethingWillHappen2()])
+  .then(response => {
+    console.log("array of results", response);
+  })
+  .catch(err => {
+    console.error(err);
+  })
